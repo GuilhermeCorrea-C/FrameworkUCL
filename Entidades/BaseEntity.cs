37d88ad1e9b8099bae443213e7e7345bf29d9787
André Aquino <andre.luiz.aquino30@gmail.com>
2023-09-20 19:57:53 -0300

    commit inicial

---
+++ Entidades/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrameworkUCL.Entidades
+{
+    public class BaseEntity
+    {
+        
+    }
+}
